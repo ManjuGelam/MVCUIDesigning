6a1f3beaaff3a271ed4aefae05ac331205a5de14
APTOL301938 <APTOL301938@APOLBO03-188>
2023-03-31 10:51:32 +0530

    Add project files.

---
+++ MVCUIDesigning/Controllers/EmpController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCUIDesigning.Controllers
+{
+    public class EmpController : Controller
+    {
+
+        [HttpGet]
+        public ViewResult AddEmp()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ViewResult AddEmp(int? id, string name, string job, double? salary)
+        {
+            ViewData["Id"] = id;
+            ViewData["Name"] = name;
+            ViewData["Job"] = job;
+            ViewData["Salary"] = salary;
+            return View("DisplayEmp1");
+        }
+
+
+    }
+}
